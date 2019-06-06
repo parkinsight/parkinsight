038b6f92df1e1ace3da73389de92aaac1d329381
annnnnieee <anastasiya.vt.s@gmail.com>
2019-06-06 15:58:50 -0700

    added a formated graph that is able to display data correctly. still needs to be refactored and hooked up to the server.

--- mobile/app/src/main/java/com/example/parkinsight/DashboardActivity.java
+++ mobile/app/src/main/java/com/example/parkinsight/DashboardActivity.java
@@ -1,84 +1,106 @@
 package com.example.parkinsight;
 
+
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.graphics.*;
-import android.os.Bundle;
+import android.widget.Toast;
 
-import com.androidplot.util.PixelUtils;
-import com.androidplot.xy.SimpleXYSeries;
-import com.androidplot.xy.XYSeries;
-import com.androidplot.xy.*;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.DataPointInterface;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.OnDataPointTapListener;
+import com.jjoe64.graphview.series.PointsGraphSeries;
+import com.jjoe64.graphview.series.Series;
 
-import java.text.FieldPosition;
-import java.text.Format;
-import java.text.ParsePosition;
-import java.util.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 
 public class DashboardActivity extends AppCompatActivity {
 
-    private XYPlot plot;
+    private GraphView graph;
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        // initialize our XYPlot reference:
-        plot = (XYPlot) findViewById(R.id.plot);
+        Calendar calendar = Calendar.getInstance();
+        Date d1 = calendar.getTime();
+        calendar.add(Calendar.DATE, 1);
+        Date d2 = calendar.getTime();
+        calendar.add(Calendar.DATE, 2);
+        Date d3 = calendar.getTime();
+        calendar.add(Calendar.DATE, 3);
+        Date d4 = calendar.getTime();
+        calendar.add(Calendar.DATE, 7);
+        Date d41 = calendar.getTime();
+        calendar.add(Calendar.DATE, 11);
+        Date d42 = calendar.getTime();
+        calendar.add(Calendar.DATE, 12);
+        Date d43 = calendar.getTime();
+        calendar.add(Calendar.DATE, 23);
+        Date d5 = calendar.getTime();
 
-        // create a couple arrays of y-values to plot:
-        final Number[] domainLabels = {1, 2, 3, 6, 7, 8, 9, 10, 13, 14};
-        Number[] series1Numbers = {1, 4, 2, 8, 4, 16, 8, 32, 16, 64};
-        Number[] series2Numbers = {5, 2, 10, 5, 20, 10, 40, 20, 80, 40};
+        graph = (GraphView) findViewById(R.id.graph);
 
-        // turn the above arrays into XYSeries':
-        // (Y_VALS_ONLY means use the element index as the x value)
-        XYSeries series1 = new SimpleXYSeries(
-                Arrays.asList(series1Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series1");
-        XYSeries series2 = new SimpleXYSeries(
-                Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2");
 
-        // create formatters to use for drawing a series using LineAndPointRenderer
-        // and configure them from xml:
-        LineAndPointFormatter series1Format =
-                new LineAndPointFormatter(this, R.xml.line_point_formatter_with_labels);
 
-        LineAndPointFormatter series2Format =
-                new LineAndPointFormatter(this, R.xml.line_point_formatter_with_labels_2);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(d1, 1),
+                new DataPoint(d2, 5),
+                new DataPoint(d3, 3),
+                new DataPoint(d4, 2),
+                new DataPoint(d41, 4),
+                new DataPoint(d42, 33),
+                new DataPoint(d43, 2),
+                new DataPoint(d5, 39)
+        });
+        graph.addSeries(series);
 
-        // add an "dash" effect to the series2 line:
-        series2Format.getLinePaint().setPathEffect(new DashPathEffect(new float[] {
+        PointsGraphSeries<DataPoint> series3 = new PointsGraphSeries<>(new DataPoint[] {
+                new DataPoint(d1, 1),
+                new DataPoint(d2, 5),
+                new DataPoint(d3, 3),
+                new DataPoint(d4, 2),
+                new DataPoint(d41, 4),
+                new DataPoint(d42, 33),
+                new DataPoint(d43, 2),
+                new DataPoint(d5, 39)
+        });
+        graph.addSeries(series3);
+        series3.setSize(10);
+        series3.setColor(Color.RED);
 
-                // always use DP when specifying pixel sizes, to keep things consistent across devices:
-                PixelUtils.dpToPix(20),
-                PixelUtils.dpToPix(15)}, 0));
+        SimpleDateFormat df = new SimpleDateFormat("MMM d, hh:mmaaa");
 
-        // just for fun, add some smoothing to the lines:
-        // see: http://androidplot.com/smooth-curves-and-androidplot/
-        series1Format.setInterpolationParams(
-                new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
+        // set date label formatter
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this, df));
+        graph.getGridLabelRenderer().setNumHorizontalLabels(5);
+        graph.getGridLabelRenderer().setHorizontalLabelsAngle(115);
 
-        series2Format.setInterpolationParams(
-                new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
+//         set manual x bounds to have nice steps
+        graph.getViewport().setMinX(d42.getTime());
+        graph.getViewport().setMaxX(d5.getTime());
+        graph.getViewport().setXAxisBoundsManual(true);
 
-        // add a new series' to the xyplot:
-        plot.addSeries(series1, series1Format);
-        plot.addSeries(series2, series2Format);
+        graph.getViewport().setScalable(true);
 
-        plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new Format() {
+        // as we use dates as labels, the human rounding to nice readable numbers
+// is not necessary
+        graph.getGridLabelRenderer().setHumanRounding(false);
+
+        series3.setOnDataPointTapListener(new OnDataPointTapListener() {
             @Override
-            public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
-                int i = Math.round(((Number) obj).floatValue());
-                return toAppendTo.append(domainLabels[i]);
-            }
-            @Override
-            public Object parseObject(String source, ParsePosition pos) {
-                return null;
+            public void onTap(Series series, DataPointInterface dataPoint) {
+                Toast.makeText(graph.getContext(), "Series1: On Data Point clicked: "+dataPoint.getY(), Toast.LENGTH_SHORT).show();
             }
         });
     }
-}
 
+}
