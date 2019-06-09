2e1be64e4102c1f1a13e194a1d2860e63e90b36f
annnnnieee <anastasiya.vt.s@gmail.com>
2019-06-09 13:25:31 -0700

    added headers

--- mobile/app/src/main/java/com/example/parkinsight/DashboardViewModel.java
+++ mobile/app/src/main/java/com/example/parkinsight/DashboardViewModel.java
@@ -1,6 +1,8 @@
 package com.example.parkinsight;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -10,13 +12,19 @@ import androidx.lifecycle.MutableLiveData;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.error.AuthFailureError;
 import com.android.volley.request.JsonObjectRequest;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class DashboardViewModel extends AndroidViewModel {
     private MutableLiveData<Scores> scores;
+    private static final String PREFERENCES = "prefrences";
+    private static final String auth = "auth_token";
 
     public DashboardViewModel(@NonNull Application application) {
         super(application);
@@ -30,15 +38,21 @@ public class DashboardViewModel extends AndroidViewModel {
         return scores;
     }
 
+    private String getAccessToken() {
+        SharedPreferences pref = getApplication().getSharedPreferences(PREFERENCES, Context.MODE_PRIVATE);
+        return pref.getString(auth, null);
+    }
+
     private void loadScores() {
-        // TODO: replace someuser..
-        String url = "https://parkinsight.azurewebsites.net/user/someuser/scores1";
+        String url = "https://parkinsight.azurewebsites.net/user/scores1";
         RequestQueue queue = RequestHandler.getInstance(getApplication()).getRequestQueue();
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, (response) -> {
                     Gson gson = new GsonBuilder()
-                            .setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+                            .setDateFormat("yyyy-MM-dd HH:mm")
+                            .create();
+                    Log.e("our response: ", response.toString());
                     Scores s = gson.fromJson(response.toString(), Scores.class);
                     scores.setValue(s);
                     Log.e("okk: ", "and the length is: " + s.scores.length);
@@ -46,7 +60,15 @@ public class DashboardViewModel extends AndroidViewModel {
                 }, error -> {
                     Log.e("ERROROROOROROOROOROROOR", "SOMETHING HORRIBLE HAPPENED WHILE LOADING SCORES");
 
-                });
+                })
+        {
+            @Override
+            public Map getHeaders() throws AuthFailureError {
+                HashMap headers = new HashMap();
+                headers.put("Authorization", "Bearer " + getAccessToken());
+                return headers;
+            }
+        };
 
         queue.add(jsonObjectRequest);
     }
--- mobile/app/src/main/java/com/example/parkinsight/VoiceActivity.java
+++ mobile/app/src/main/java/com/example/parkinsight/VoiceActivity.java
@@ -1,5 +1,8 @@
 package com.example.parkinsight;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
@@ -16,6 +19,7 @@ import android.widget.Toast;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.error.AuthFailureError;
 import com.android.volley.error.VolleyError;
 import com.android.volley.request.SimpleMultiPartRequest;
 
@@ -23,6 +27,8 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -33,17 +39,18 @@ public class VoiceActivity extends AppCompatActivity {
     private MediaRecorder recorder;
     private Button recordButton;
     private String outputfile, extName, filePath;
-    private TextView textView;
     private File directory = new File(Environment.getExternalStorageDirectory() + "/Test Recordings");
     private SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
     private Handler mHandler = new Handler();
     private RequestQueue queue;
 
+    private static final String PREFERENCES = "prefrences";
+    private static final String auth = "auth_token";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voice);
-        textView = (TextView) findViewById(R.id.textview);
         outputfile = sdf.format(new Date());
         extName = ".wav";
         queue = RequestHandler.getInstance(this).getRequestQueue();
@@ -54,7 +61,6 @@ public class VoiceActivity extends AppCompatActivity {
         recordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView.setText(recordButton.getText().toString());
                 if (recordButton.getText().equals("Record")) {
                     recordButton.setText("RECORDING");
                     directory.mkdirs();
@@ -90,7 +96,8 @@ public class VoiceActivity extends AppCompatActivity {
                                                 recorder.stop();
                                                 postVoiceRecording();
                                                 Toast.makeText(VoiceActivity.this, "The audio file is saved to " + filePath, Toast.LENGTH_LONG).show();
-
+                                                Intent intent = new Intent(VoiceActivity.this, DashboardActivity.class);
+                                                startActivity(intent);
                                             }
                                         }
                                     });
@@ -105,6 +112,11 @@ public class VoiceActivity extends AppCompatActivity {
         });
     }
 
+    private String getAccessToken() {
+        SharedPreferences pref = getSharedPreferences(PREFERENCES, Context.MODE_PRIVATE);
+        return pref.getString(auth, null);
+    }
+
     private void postVoiceRecording() {
 //      {\ /}
 //      (•-•)
@@ -116,14 +128,24 @@ public class VoiceActivity extends AppCompatActivity {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        Log.d("Response", response);
+                        Log.e("Response", response);
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Toast.makeText(getApplicationContext(), "error: " + error.getMessage(), Toast.LENGTH_LONG).show();
             }
-        });
+        })
+                {
+                    @Override
+                    public Map getHeaders() throws AuthFailureError {
+                        HashMap headers = new HashMap();
+                        headers.put("Authorization", "Bearer " + getAccessToken());
+                        return headers;
+                    }
+                };
+
+
 
         smr.addFile("file", filePath);
         queue.add(smr);
